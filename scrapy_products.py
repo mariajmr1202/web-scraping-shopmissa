17c4e0ac98288723abbc103b6abee64403452264
mariajmr1202 <mariajmr1202@gmail.com>
2021-09-14 02:28:18 -0400

    Se agrego funcion que descarga las imagenes de los productos y las guarda localmente

--- scrapy_products.py
+++ scrapy_products.py
@@ -1,7 +1,6 @@
 import requests
 from PIL import Image
 import io
-from scrapy.http import request
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.crawler import CrawlerProcess
 from scrapy.linkextractors import LinkExtractor
@@ -16,7 +15,19 @@ class ShopMissA(CrawlSpider):
 
     allowed_domains = ['shopmissa.com']
 
-    start_urls = ['https://www.shopmissa.com/collections/skincare']
+    start_urls = [
+        'https://www.shopmissa.com/collections/skincare',
+        'https://www.shopmissa.com/collections/oki-life',
+        'https://www.shopmissa.com/collections/spa-body',
+        'https://www.shopmissa.com/collections/blenders-sponges',
+        'https://www.shopmissa.com/collections/makeup-brushes',
+        'https://www.shopmissa.com/collections/eyes',
+        'https://www.shopmissa.com/collections/lips',
+        'https://www.shopmissa.com/collections/face-body',
+        'https://www.shopmissa.com/collections/nails',
+        'https://www.shopmissa.com/collections/tools',
+        'https://www.shopmissa.com/collections/makeup-pouches-bags'
+    ]
 
     rules = (
         #Paginacion
@@ -45,10 +56,27 @@ class ShopMissA(CrawlSpider):
             description += text
         return description
 
+    #Funcion que encuentra la categoria del producto
     def find_categorie(self, url):
         categorie = str(url).replace('<200 https://www.shopmissa.com/collections/','')
         categorie = categorie[:categorie.find('/')].replace('oki-life','Life & Home').replace('spa-body','Spa & Body').replace('-',' ').title()
         return categorie
+
+    #Funcion que extrae y descarga las imagenes de lo productos
+    def scrapy_images(self, urls, product):
+        try:
+            i = 1
+            for url in urls:
+                image_content = requests.get('https:'+url).content   
+                image_file = io.BytesIO(image_content)
+                image = Image.open(image_file).convert('RGB')
+                file_path = './images/'+ product.replace('-','').replace('+',' ').replace(' ','_') +'_'+str(i)+'.jpg'  # nombre a guardar de la imagen
+                with open(file_path, 'wb') as f:
+                    image.save(f, "JPEG", quality=85)   
+                i+=1
+        except Exception as e:
+            print(e)
+            print ("Error") 
     
     def parse_product(self, response):
         # Extraer nombre del producto
@@ -59,26 +87,18 @@ class ShopMissA(CrawlSpider):
         description = self.join_description(text_p, text_li) 
         # Extraer categoria del producto   
         categorie =  self.find_categorie(response)
-        #Extraer imagen
-        images = response.xpath('//div[@class="AspectRatio AspectRatio--withFallback"]//img/@data-original-src').get()
+        #Extraer url de imagenes
+        images_url = response.xpath('//div[@class="AspectRatio AspectRatio--withFallback"]//img/@data-original-src').getall()
 
         products.append({
             "name": name,
             "description": description,
             "categorie" : categorie,
-            "images": images,
+            "images": images_url,
         })
 
-        try:
-            image_content = requests.get('https:'+images).content   
-            image_file = io.BytesIO(image_content)
-            image = Image.open(image_file).convert('RGB')
-            file_path = './images/'+ name.replace('-',' ').replace('+',' ').replace(' ','_') + '.jpg'  # nombre a guardar de la imagen
-            with open(file_path, 'wb') as f:
-                image.save(f, "JPEG", quality=85)   
-        except Exception as e:
-            print(e)
-            print ("Error")  
+        # self.scrapy_images(images_url, name)
+        
 
 products = []
 
@@ -86,13 +106,16 @@ process = CrawlerProcess()
 process.crawl(ShopMissA)
 process.start()
 
-print('\n\n')
-print('Productos:')
-print('\n\n')
+# print('\n\n')
+# print('Productos:')
+# print('\n\n')
 
+# i = 1
 # for product in products:
+#     print(i)
 #     print('Name: '+ product['name'])
 #     print(product['description'])
 #     print('Categorie: '+product['categorie'])
 #     print(product['images'])
+#     i+=1
 #     print('\n')
